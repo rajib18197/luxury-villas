f4c57483a85379a7937b4788caad6e5a6c6bc05a
rajib18197 <rajib18197@gmail.com>
2021-07-20 11:29:03 +0600

    first-commit

---
+++ js/navigation.js
@@ -0,0 +1,8 @@
+const header = document.querySelector(".header");
+const menu = document.querySelector(".header__menu");
+const overlay = document.querySelector(".header__overlay");
+menu.addEventListener("click", function () {
+  menu.classList.toggle(".open");
+  header.classList.toggle("open");
+  overlay.classList.toggle(".open");
+});
